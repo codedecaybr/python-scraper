703abde4391ce6fa85f139b28232e32a6e062402
Renan Yuri Lino <renan.lino@gmail.com>
2017-08-30 21:24:25 +0000

    filtro e download de páginas individuais de vagas

--- teste1.py
+++ teste1.py
@@ -2,19 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 n = 1503614732084
 URL = "https://www.indeed.com.br/empregos?q=programador&l=S%C3%A3o+Paulo%2C+SP&start=##START_INDEX##"
+baseURL = "https://www.indeed.com.br"
 links = []
 for i in range(1,4):
     rURL = URL.replace("##START_INDEX##", str(i*10) )
-    print(rURL)
+    print("Baixando página de pesquisa: " + rURL)
     page = requests.get(rURL)
     soup = BeautifulSoup(page.text, 'html.parser')
     res = soup.find_all(class_="turnstileLink")
     for i in range(len(res)):
-        link = res[i].get("href")
-        print(link, end="")
-        if link in links:
-            print(" REPETIDO")
-        else:
-            print(" NOVO")
-            links.append(link)
+        if res[i].get("title") is None:
+            continue
+        print("\tBaixando página da vaga: " + res[i].get("title"))
+        jobPage = requests.get(baseURL + res[i].get("href"))
+        jobSoup = BeautifulSoup(jobPage.text, 'html.parser')
+        ps = soup.find_all(class_="p")
+        for p in ps:
+            if "Salário" in p:
+                print (p.get_text())
     input()
