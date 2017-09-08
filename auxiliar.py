f252f3f586dbdcf4028919077e86b66eebbd83ec
Renan Yuri Lino <renan.lino@gmail.com>
2017-09-08 21:25:00 +0000

    Adiciona visualização de dados

--- auxiliar.py
+++ auxiliar.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 class Requisito:
 
     def __init__(self, nome):
@@ -10,9 +14,49 @@ class Requisito:
         if salario > 0:
             self.salarios.append(salario)
 
+    def salarioMedio(self):
+        if len(self.salarios) > 0:
+            return sum(self.salarios)/len(self.salarios)
+        else:
+            return 0
+
+
     def printEstat(self):
         print(self.nome)
         print("\tFrequência: %d" %(self.ocorrencias))
         print("\tVagas com salário: %d" %(len(self.salarios)))
         if len(self.salarios) > 0:
-            print("\tMédia de salário: R$ %.2f" %(sum(self.salarios)/len(self.salarios)) )
+            print("\tMédia de salário: R$ %.2f" %( self.salarioMedio() ))
+
+def ScrapView(reqs):
+    y1 = []
+    y2 = []
+    x = []
+    for req in reqs:
+        y1.append(reqs[req].salarioMedio())
+        y2.append(reqs[req].ocorrencias)
+        x.append(req)
+
+    index = np.arange(len(reqs))
+    width = 0.35
+
+    fig, ax = plt.subplots()
+    plt.xticks(rotation=70)
+    rects1 = ax.bar(index, y1, width, color='r')
+    ax.set_xticks(index)
+    ax.set_xticklabels(x)
+    ax.set_title('Salário Médio dos Requisitos de Emprego no Indeed')
+    plt.xlabel('Requisito')
+    plt.ylabel('Salário Médio (R$)')
+
+    fig, ax = plt.subplots()
+    plt.xticks(rotation=70)
+    rects2 = ax.bar(index, y2, width, color='b')
+    ax.set_xticks(index)
+    ax.set_xticklabels(x)
+    ax.set_title('Frequência dos Requisitos de Emprego no Indeed')
+    plt.xlabel('Requisito')
+    plt.ylabel('Frequência')
+
+
+    plt.show()
